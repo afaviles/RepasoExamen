f36bcaf9ae8806d9e498c218e1516503a35b4218
afaviles <3210090@alu.murciaeduca.es>
2023-05-16 22:13:17 +0200

    Radar inicial

---
+++ src/radar/Radar.java
@@ -0,0 +1,66 @@
+package radar;
+
+import java.util.Scanner;
+
+public class Radar {
+
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float recorrido = 0F;
+
+        float tiempo = 0F;
+
+        float veloci = 0F;
+
+        int difVelocidad = 0;
+
+        int velMax = 0;
+
+        int puntos = 0;
+
+        recorrido = sc.nextFloat();
+
+        velMax = sc.nextInt();
+
+        tiempo = sc.nextFloat();
+
+        recorrido = recorrido / 1000;
+
+        tiempo = tiempo / 3600;
+
+        veloci = recorrido / tiempo;
+
+
+
+        difVelocidad = (int) ((veloci - velMax) / 10);
+
+        if (veloci <= velMax) {
+
+            System.out.println("OK");
+
+        } else {
+
+            if (difVelocidad == 0) {
+
+                puntos = 0;
+            } else if (difVelocidad == 1) {
+
+                puntos = 2;
+            } else if (difVelocidad == 2) {
+
+                puntos = 4;
+            } else if (difVelocidad == 3) {
+
+                puntos = 7;
+            } else if (difVelocidad == 4) {
+
+                puntos = 10;
+            }
+            System.out.println("MULTA - " + puntos + " puntos");
+
+        }
+
+    }
+
+}
